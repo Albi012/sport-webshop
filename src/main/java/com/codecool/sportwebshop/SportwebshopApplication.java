f66057e7fbd4e1920584cdb5b5ca5237c4b68435
Albi012 <mayeralbert12@gmail.com>
2020-11-21 12:19:17 +0100

    Products.

--- src/main/java/com/codecool/sportwebshop/SportwebshopApplication.java
+++ src/main/java/com/codecool/sportwebshop/SportwebshopApplication.java
@@ -32,7 +32,6 @@ public class SportwebshopApplication {
                     .price(55)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/1.png?alt=media&token=c5b67ea2-6ebb-458a-9d23-c2ca109e6eaa")
                     .productType(ProductType.HOODIE)
-                    .size(List.of("XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product1);
             Product product2 = Product.builder()
@@ -40,7 +39,6 @@ public class SportwebshopApplication {
                     .price(70)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/2.png?alt=media&token=b0889fdc-dd62-4b96-a612-baf6e710c23c")
                     .productType(ProductType.HOODIE)
-                    .size(List.of("XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product2);
             Product product3 = Product.builder()
@@ -48,7 +46,6 @@ public class SportwebshopApplication {
                     .price(65)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/6.png?alt=media&token=99fc439b-2af7-4ff0-beb5-0929cf397964")
                     .productType(ProductType.HOODIE)
-                    .size(List.of("XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product3);
             Product product4 = Product.builder()
@@ -56,7 +53,6 @@ public class SportwebshopApplication {
                     .price(100)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/3.png?alt=media&token=db98cb96-fb06-414b-b537-4ccdd653996a")
                     .productType(ProductType.HOODIE)
-                    .size(List.of("XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product4);
             Product product5 = Product.builder()
@@ -64,7 +60,6 @@ public class SportwebshopApplication {
                     .price(80)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/4.png?alt=media&token=de43c999-8970-4dc1-a3e3-f54dd8e60b5a")
                     .productType(ProductType.HOODIE)
-                    .size(List.of("XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product5);
             Product product6 = Product.builder()
@@ -72,7 +67,6 @@ public class SportwebshopApplication {
                     .price(100)
                     .imgSrc("https://firebasestorage.googleapis.com/v0/b/sport-webshop.appspot.com/o/5.png?alt=media&token=4d6f2f61-f278-4ee4-bed6-92bef0fc357c")
                     .productType(ProductType.OTHER)
-                    .size(List.of("XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"))
                     .build();
             productRepository.saveAndFlush(product6);
         };
